b3d950be313a678380fe233cbf32998204021d50
Jfuentes4 <jfuentes4@ucol.mx>
2018-10-09 19:06:55 -0500

    terminado

--- Unit3/ejemplo-3/src/js/ejemplo-3.js
+++ Unit3/ejemplo-3/src/js/ejemplo-3.js
@@ -1,10 +1,35 @@
-
 var btnCalcular = document.querySelector('#btnEvaluar')
 var pResultado = document.querySelector('#resultado')
 var opciones = document.querySelector('#opciones')
 var inputNumero = document.querySelector('#numero1')
 btnCalcular.addEventListener('click', evaluarNumero)
-opciones.addEventListener('change', evalOpcion)
+opciones.addEventListener('change', evalOpcionSwitch)
+
+function evalOpcionSwitch () {
+  var numero = Number(inputNumero.value)
+  var opcion = opciones.value
+
+  switch (opcion) {
+    case 'mitad':
+      pResultado.textContent = 'La mitad es ' + (numero / 2)
+      break
+    case 'cuadrado':
+      pResultado.textContent = 'El cuadrado del numero es ' + Math.pow(numero, 2)
+      break
+    case 'cubo':
+      pResultado.textContent = 'El cubo del numero es ' + Math.pow(numero, 3)
+      break
+    case 'par':
+      if (numero % 2 === 0) {
+        pResultado.textContent = 'El numero es par'
+      } else {
+        pResultado.textContent = 'El numero es impar'
+      }
+      break
+    default:
+      pResultado.textContent = ''
+  }
+}
 
 function evalOpcion () {
   var numero = Number(inputNumero.value)
@@ -13,9 +38,9 @@ function evalOpcion () {
   if (opcion === 'mitad') {
     pResultado.textContent = 'La mitad es ' + (numero / 2)
   } else if (opcion === 'cuadrado') {
-    pResultado.textContent = 'La mitad es ' + Math.pow(numero, 2)
+    pResultado.textContent = 'El cuadrado del numero es ' + Math.pow(numero, 2)
   } else if (opcion === 'cubo') {
-    pResultado.textContent = 'La mitad es ' + Math.pow(numero, 3)
+    pResultado.textContent = 'El cubo del numero es ' + Math.pow(numero, 3)
   } else if (opcion === 'par') {
     if (numero % 2 === 0) {
       pResultado.textContent = 'El numero es par'
